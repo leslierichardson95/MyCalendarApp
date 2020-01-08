fe09cc437c01e42dc3732b82e8d9889f26112c75
Leslie Richardson <lerich@microsoft.com>
2020-01-08 14:53:25 -0800

    Add project files.

---
+++ MyCalendarApp/Models/Event.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCalendarApp.Models
+{
+    // Class representing custom events and events added from Ticketmaster API
+    public class Event
+    {
+        // Name of the event
+        [JsonProperty("title", NullValueHandling = NullValueHandling.Ignore)]
+        private string Title { get; set; }
+
+        // Description of the event
+        [JsonProperty("description", NullValueHandling = NullValueHandling.Ignore)]
+        private string Description { get; set; }
+
+        // The color the event will be displayed as in the calendar
+        [JsonProperty("color", NullValueHandling = NullValueHandling.Ignore)]
+        private string Color { get; set; }
+
+        // Is the event a day/multiple-day event?
+        [JsonProperty("isFullDay", NullValueHandling = NullValueHandling.Ignore)]
+        private bool IsFullDay { get; set; }
+
+        // The event's start date and start time
+        [JsonProperty("startTime", NullValueHandling = NullValueHandling.Ignore)]
+        private DateTime StartTime { get; set; }
+
+        // The event's end date and end time
+        [JsonProperty("endTime", NullValueHandling = NullValueHandling.Ignore)]
+        private DateTime EndTime { get; set; }
+
+        // An image representing the event
+        [JsonProperty("image", NullValueHandling = NullValueHandling.Ignore)]
+        private string Image { get; set; }
+
+        // Custom tags associated with the event
+        [JsonProperty("tags", NullValueHandling = NullValueHandling.Ignore)]
+        private List<string> Tags { get; set; }
+
+        // Was the event made by the user (true) or is it from TicketMaster (false)?
+        [JsonProperty("isCustomEvent", NullValueHandling = NullValueHandling.Ignore)]
+        private bool IsCustomEvent { get; set; }
+    }
+}
---
+++ MyCalendarApp/wwwroot/js/calendar-script.js
@@ -0,0 +1,89 @@
+﻿$(document).ready(function () {
+    var selectedEvent = null;
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        firstDay: 1, //The day that each week begins (Monday=1)
+        plugins: ['bootstrap'],
+        themeSystem: 'bootstrap',
+        slotMinutes: 60,
+        eventLimit: true,
+        eventClick:
+            function (calEvent, jsEvent, view) {
+                selectedEvent = calEvent;
+                $('#eventInfoModal #eventTitle').text(calEvent.title);
+                var $description = $('<div/>');
+                $description.append($('<p/>').html('<b>Start:</b>' + calEvent.start.format("MMM-DD-YYYY HH:mm a")));
+                if (calEvent.end !== null) {
+                    $description.append($('<p/>').html('<b>End:</b>' + calEvent.end.format("MMM-DD-YYYY HH:mm a")));
+                }
+                $description.append($('<p/>').html('<b>Description:</b>' + calEvent.description));
+                $('#eventInfoModal #pDetails').empty().html($description);
+
+                $('#eventInfoModal').modal();
+            },
+        selectable: true,
+        select: function (start, end) {
+            selectedEvent = {
+                eventID: 0,
+                title: '',
+                description: '',
+                start: start,
+                end: end,
+                allDay: false,
+                color: ''
+            };
+            openAddEditForm();
+            $('#calendar').fullCalendar('unselect');
+        },
+        editable: true,
+        eventDrop: function (event) {
+            var data = {
+                EventID: event.eventID,
+                Subject: event.title,
+                Start: event.start.format('MM/DD/YYYY HH:mm A'),
+                End: event.end !== null ? event.end.format('MM/DD/YYYY HH:mm A') : null,
+                Description: event.description,
+                ThemeColor: event.color,
+                IsFullDay: event.allDay
+            };
+            //SaveEvent(data);
+        }
+    });
+
+    // TODO: Figure out why clock doesn't display
+    $('#txtStart,#dtp2').datetimepicker({
+        format: 'MM/DD/YYYY HH:mm A'
+    });
+
+    $('#chkIsFullDay').change(function () {
+        if ($(this).is(':checked')) {
+            $('#divEndDate').hide();
+        }
+        else {
+            $('#divEndDate').show();
+        }
+    });
+
+    function openAddEditForm() {
+        if (selectedEvent !== null) {
+            $('#hdEventID').val(selectedEvent.eventID);
+            $('#txtSubject').val(selectedEvent.title);
+
+            //TODO: Format to display in date input box
+            $('#txtStart').val(selectedEvent.start.format('MM/DD/YYYY HH:mm A'));
+
+            $('#chkIsFullDay').prop("checked", selectedEvent.allDay || false);
+            $('#chkIsFullDay').change();
+            //$('#txtEnd').val(selectedEvent.end !== null ? selectedEvent.end.format('MM/DD/YYYY HH:mm A') : '');
+            $('#txtEnd').val(selectedEvent.start.format('MM/DD/YYYY HH:mm A'));
+            $('#txtDescription').val(selectedEvent.description);
+            $('#ddThemeColor').val(selectedEvent.color);
+        }
+        $('#eventInfoModal').modal('hide');
+        $('#addEditEventModal').modal();
+    }
+}); 
