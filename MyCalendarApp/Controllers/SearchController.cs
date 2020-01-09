5d84e3a5671a1e1304f607ed27ab453b158578e7
Leslie Richardson <lerich@microsoft.com>
2020-01-09 09:49:50 -0800

    Added Find Events page

---
+++ MyCalendarApp/Controllers/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyCalendarApp.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ILogger<SearchController> _logger;
+
+        public SearchController(ILogger<SearchController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult SearchEvents()
+        {
+            return View();
+        }
+    }
+}
